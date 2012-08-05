a8e126f31a8a92cf2d44717eac0efa4d10f4244c
Corey Leigh Latislaw <colabug@gmail.com>
2012-08-05 17:36:31 -0400

    Hello world

---
+++ src/com/colabug/calc/CalculatorActivity.java
@@ -0,0 +1,15 @@
+package com.colabug.calc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
